b07b01f3966e829fad163582a373db93925bced6
nathan bruzat <nathan.bruzat@viacesi.fr>
2019-03-27 11:08:52 +0100

    fix save

--- method/proximalPolicyOptimization.py
+++ method/proximalPolicyOptimization.py
@@ -26,7 +26,7 @@ class ProximalPolicyOptimization(baseMethod.BaseMethod):
 
     def save(self,path, name):
         super().save(path, name)
-        self.critic.save_model(path)
+        self.critic.save_model(path+'critic/'+name)
 
     def load(self, path, name):
         super().load(path, name)
